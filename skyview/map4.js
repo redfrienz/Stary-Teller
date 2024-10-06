9af4372eeb92aed34c0e01d2a8420f134e935fb5
dgkim0329 <dgkim0329@gmail.com>
2024-10-06 22:37:38 +0900

    Update map4.js

--- skyview/map4.js
+++ skyview/map4.js
@@ -237,7 +237,7 @@ window.addEventListener('DOMContentLoaded', () => {
                             .html(tooltipText);
                     }
                 });
-            const connections = JSON.parse(localStorage.getItem('ownConstellation')) || [];
+            const connections = JSON.parse(localStorage.getItem('ownConstellation4')) || [];
             drawConnections(data, connections);
         });
 
